bba478f466b64b9108f47434db002b1d1487e9fb
Cloxl <Cloxl2017@outlook.at>
2024-10-26 17:28:51 +0800

    refactor(extract): 使用py提取js转json 去除node js依赖

--- extractor/extract_initial_state.py
+++ extractor/extract_initial_state.py
@@ -1,19 +1,17 @@
 import json
 import re
 from typing import Any
-
-import execjs
 from bs4 import BeautifulSoup
 from loguru import logger
 
 
-def extract_initial_state(html_content: str, output_json_path: str) -> Any | None:
+async def extract_initial_state(html_content: str, replacements: dict) -> Any | None:
     """
     从HTML中的<script>标签中提取window.__INITIAL_STATE__ 解析并保存为JSON格式
 
     如果未找到 window.__INITIAL_STATE__ 函数会返回 None 并输出响应的HTML内容
-    :param html_content: str html文档内容
-    :param output_json_path: str 输出保存JSON的文件路径
+    :param html_content: html文档内容
+    :param replacements: js对象转json需要替换的字符串对应关系
 
     :return: dict: 提取并格式化后的JSON数据
     """
@@ -33,12 +31,12 @@ def extract_initial_state(html_content: str, output_json_path: str) -> Any | Non
                 initial_state_str = match.group(1)
                 break
 
-    # 找到初始状态字符串
+    # 栈空间保证先入后出 可以确保提取所有的大括号
+    # 具体原理查看 https://leetcode.cn/problems/valid-parentheses/description/
     if initial_state_str:
         stack = []
         result = []
 
-        # 解析大括号 {} 来提取内容
         for char in initial_state_str:
             if char == '{':
                 stack.append('{')
@@ -52,10 +50,10 @@ def extract_initial_state(html_content: str, output_json_path: str) -> Any | Non
 
         extracted_content = ''.join(result)
 
-        # html提取的内容为js obj需要通过js序列化
-        with open('./static/formatJson.js', 'r', encoding='utf-8') as js_file:
-            return json.loads(execjs.compile(js_file.read()).call('get_jc', extracted_content))
+        for old, new in replacements.items():
+            extracted_content = extracted_content.replace(old, new)
 
+        return json.loads(extracted_content)
     else:
         # 如果未找到, 说明请求出问题 可能账户被特征了
         logger.error("未找到 window.__INITIAL_STATE__")
