0b89b6d3e60f3d3a54b9760d42c1286f7a2e5456
Cloxl <Cloxl2017@outlook.at>
2024-11-05 11:03:34 +0800

    feat(code): 新增小红书验证码des key

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+class XhsDesKeys:
+    DECODE_CAPTCHA_INFO: str = "76a2171c"
+    ENCODE_MOUSE_END: str = "WquqhEkd"
+    ENCODE_TIME: str = "vPMvCY4K"
+    ENCODE_TRACK: str = "PYrm8rMk"
+    ENCODE_WIDTH: str = "WquqhEkd"
